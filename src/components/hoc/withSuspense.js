56cf9bb1d0b5b22eeef7216d3e261490c46ad65d
Vladyslav Vasylenko <vladyslav.vasylenko@dev.pro>
2020-08-14 16:04:22 +0300

    delete unused files

--- src/components/hoc/withSuspense.js
+++
@@ -1,7 +0,0 @@
-import React, { Suspense } from 'react';
-
-export const withSuspense = (Component) => (props) => (
-  <Suspense fallback={<div>Loading...</div>}>
-    <Component {...props} />
-  </Suspense>
-);
